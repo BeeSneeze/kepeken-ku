968e57d1052d3e20e76f8975ac1c2e343d65329b
Johannes Kung <johku144@student.liu.se>
2024-11-29 22:02:53 +0100

    On displaying unicode characters

--- Scripts/custom_label.cs
+++ Scripts/custom_label.cs
@@ -6,11 +6,12 @@ public class custom_label : Label
 	// Declare member variables here. Examples:
 	// private int a = 2;
 	// private string b = "text";
+	[Export] public string Message;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
+		Text = latin_to_sitelen(Message);
 	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,4 +19,17 @@ public class custom_label : Label
 //  {
 //      
 //  }
+	public string latin_to_sitelen(string word)
+	{
+		switch (word)
+		{
+			case "ku":
+				return "";
+				break;
+				
+		}
+		return "";
+	}
+	
+	//public override void _
 }
